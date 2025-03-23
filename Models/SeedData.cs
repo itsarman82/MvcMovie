691bab6ac5f260b76cf87656670e9c86b67f58a1
arman <armans8282@gmail.com>
2025-03-24 01:56:56 +0330

    Added rating database field

--- Models/SeedData.cs
+++ Models/SeedData.cs
@@ -23,6 +23,7 @@ public static class SeedData
                 new Movie
                 {
                     Title = "When Harry Met Sally",
+                    Rating = "R",
                     ReleaseDate = DateTime.Parse("1989-2-12"),
                     Genre = "Romantic Comedy",
                     Price = 7.99M
@@ -30,6 +31,7 @@ public static class SeedData
                 new Movie
                 {
                     Title = "Ghostbusters ",
+                    Rating = "R",
                     ReleaseDate = DateTime.Parse("1984-3-13"),
                     Genre = "Comedy",
                     Price = 8.99M
@@ -37,6 +39,7 @@ public static class SeedData
                 new Movie
                 {
                     Title = "Ghostbusters 2",
+                    Rating = "R",
                     ReleaseDate = DateTime.Parse("1986-2-23"),
                     Genre = "Comedy",
                     Price = 9.99M
@@ -44,6 +47,7 @@ public static class SeedData
                 new Movie
                 {
                     Title = "Rio Bravo",
+                    Rating = "R",
                     ReleaseDate = DateTime.Parse("1959-4-15"),
                     Genre = "Western",
                     Price = 3.99M
--- obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adb69c84c6d933cff54d8940417c78d2484caedf")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
